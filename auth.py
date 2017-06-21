acf524ac1b8455303b9e0d08a514f04e5a6913b7
Ian Bebbington <github@bebbs.co.uk>
2017-06-21 14:47:36 +0100

    Revoked twitter credentials

--- auth.py
+++ auth.py
@@ -1,3 +1,5 @@
+# Revoked - retrieve new ones
+
 consumer_key        = 'FCrKktVv6fCn8ljSan8TWY10J'
 consumer_secret     = 'zfLyDV0PxBpmHZmZOPUeICxzcvJpvuFMGqss1lzC6K4kYTv626'
 access_token        = '877452419642388480-3bHZw9prKLLA2QcqBm5pl2LFg0c5RUO'
