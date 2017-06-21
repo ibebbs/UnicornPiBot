cc8f4a1267ca48ca960a76b1d59bc6cc95576614
Ian Bebbington <github@bebbs.co.uk>
2017-06-21 14:38:06 +0100

    Initial commit

---
+++ UnicornPiBot.py
@@ -0,0 +1,32 @@
+import sys
+import random
+
+from UHScroll import *
+from twython import TwythonStreamer
+from auth import (
+    consumer_key,
+    consumer_secret,
+    access_token,
+    access_token_secret
+)
+
+colours = ['red','white','pink','blue','green','cyan']
+
+non_bmp_map = dict.fromkeys(range(0x10000, sys.maxunicode + 1), 0xfffd)
+
+class UnicornPiBotStreamer(TwythonStreamer):
+    def on_success(self, data):
+        if 'text' in data:
+            text = data['text'].translate(non_bmp_map)
+            colour = random.choice(colours)
+            print(text)
+            unicorn_scroll(text, colour, 200, 0.05)
+
+stream = UnicornPiBotStreamer(
+    consumer_key,
+    consumer_secret,
+    access_token,
+    access_token_secret
+)
+
+stream.statuses.filter(track='#DayOfRage')
---
+++ auth.py
@@ -0,0 +1,4 @@
+consumer_key        = 'FCrKktVv6fCn8ljSan8TWY10J'
+consumer_secret     = 'zfLyDV0PxBpmHZmZOPUeICxzcvJpvuFMGqss1lzC6K4kYTv626'
+access_token        = '877452419642388480-3bHZw9prKLLA2QcqBm5pl2LFg0c5RUO'
+access_token_secret = '75y1nEOsY5ASNCRar1K4LR3SZq0f4pvViyJnXdjoAhddH'
